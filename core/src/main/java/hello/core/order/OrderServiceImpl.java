8470641b7f1485706092aaeca1d915f8123a5da0
jonghunbaek <qor6028@naver.com>
2023-04-21 21:17:03 +0900

    feat : @ComponentScan을 활용해 스프링 컨테이너에 빈 자동 등록

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -7,12 +10,14 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy; // -> 인터페이스에만 의존
 	//private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); -> 인터페이스뿐만 아니라 구현체에도 의존
 	
+	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
@@ -24,5 +29,10 @@ public class OrderServiceImpl implements OrderService {
 		int discountPrice = discountPolicy.discount(member, itemPrice);
 		
 		return new Order(memberId, itemName, itemPrice, discountPrice);
+	}
+	
+	// test 용도
+	public MemberRepository getMemberRepository() {
+		return memberRepository;
 	}
 }
