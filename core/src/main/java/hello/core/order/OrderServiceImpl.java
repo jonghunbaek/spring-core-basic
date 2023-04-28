436969830c75b85dfddb97f721b8f273a997ff57
jonghunbaek <qor6028@naver.com>
2023-04-28 20:58:06 +0900

    feat : 조회된 빈이 2개 이상일 경우 해결방법 3가지

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
@@ -8,11 +10,10 @@ import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 	private final MemberRepository memberRepository;
-	private final DiscountPolicy discountPolicy; // -> 인터페이스에만 의존
+	private final  DiscountPolicy discountPolicy; // -> 인터페이스에만 의존
 	//private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); -> 인터페이스뿐만 아니라 구현체에도 의존
 	
 	/*
@@ -27,13 +28,13 @@ public class OrderServiceImpl implements OrderService {
 	}
 	*/
 	
-	/*
+	
 	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
-	*/
+	
 	
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
 
 public class AutoAppConfigTest {
 	@Test
@@ -15,5 +17,9 @@ public class AutoAppConfigTest {
 		
 		MemberService memberService = ac.getBean(MemberService.class);
 		Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+		
+		OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+		MemberRepository memberRepository = bean.getMemberRepository();
+		System.out.println("memberRepository = " + memberRepository);
 	}
 }
